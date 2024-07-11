66fd4e6a08952bccd983b452656a8482d1eba4ad
Shrooxie <121855012+Shrooxie@users.noreply.github.com>
2024-07-11 15:12:20 +0200

    Create sadasd.js

---
+++ sadasd.js
@@ -0,0 +1,21 @@
+class Car extends React.Component {
+  render() {
+    return <h2>Hi, I am a Car!</h2>;
+  }
+}
+
+function Car(props) {
+  return <h2>I am a { props.brand }!</h2>;
+}
+
+function Garage() {
+  return (
+    <>
+      <h1>Who lives in my garage?</h1>
+      <Car brand="Ford" />
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Garage />);
