8e6a80a28c760438c973d298cdbb147d6ac5f6ef
Shrooxie <121855012+Shrooxie@users.noreply.github.com>
2024-07-11 15:06:55 +0200

    Create hi.js

---
+++ hi.js
@@ -0,0 +1,20 @@
+const name = "W3Schools";
+let letter = name[2];
+
+
+let x = 123;
+x.toString();
+(123).toString();
+(100 + 23).toString();
+
+
+const d = new Date();
+d.getFullYear();
+
+
+// Create a Map
+const fruits = new Map([
+  ["apples", 500],
+  ["bananas", 300],
+  ["oranges", 200]
+]);
